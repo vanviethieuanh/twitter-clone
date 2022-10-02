9e86a55227dad1ff0c8cdca30a28054c9dc8fa5b
Văn Viết Hiếu Anh <vvha@netcompany.com>
2022-10-02 16:34:52 +0700

    Config mariadb

--- server/server/settings.py
+++ server/server/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE' : 'django.db.backends.mysql',
+		'NAME' : os.environ.get('SQL_NAME', 'Twitter'),
+		'HOST' : os.environ.get('SQL_HOST', '127.0.0.1'),
+		'PORT' : os.environ.get('SQL_PORT', '3306'),
+		'USER' : os.environ.get('SQL_USER', 'root'),
+		'PASSWORD' : os.environ.get('MYSQL_ROOT_PASSWORD', 'Ascending123'),
     }
 }
 
