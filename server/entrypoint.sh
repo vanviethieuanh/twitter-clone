0fb1a787e514c46354526cd00dbde387161fbe8e
Van Viet Hieu Anh <vanviethieuanh@gmail.com>
2022-10-21 13:14:19 +0700

    Create super user

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -6,5 +6,8 @@ python3 ./manage.py collectstatic
 echo "Making migration ..."
 python3 ./manage.py migrate
 
+echo "Creating superuser ..."
+python3 ./manage.py createsuperuser --username admin --no-input
+
 # Run uWSGI
 uwsgi --ini /app/uwsgi.ini --module server.wsgi
