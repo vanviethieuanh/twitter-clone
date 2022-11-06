c611db3c1babcb2681006980b290668e42410085
Van Viet Hieu Anh <vanviethieuanh@gmail.com>
2022-11-06 18:11:15 +0700

    Index API route swagger

--- server/server/urls.py
+++ server/server/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
             cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', SCHEMA_VIEW.with_ui('redoc',
             cache_timeout=0), name='schema-redoc'),
+    path(r'', SCHEMA_VIEW.with_ui('swagger',
+                                  cache_timeout=0), name='schema-swagger-ui'),
 
     # Admin site
     path('admin/', admin.site.urls),
